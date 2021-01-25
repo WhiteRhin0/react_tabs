43986433c77eb209e0e3c68f4ab08095bf436eaa
Oleksii Spalylo <white.rh42@gmail.com>
2021-01-26 01:17:01 +0200

    Updates

---
+++ precache-manifest.da9d31d45eb2f928bc82b466ec188a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f374280a1afec8145656529fbbc7b8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f2d8d11faeb91ad67fd",
+    "url": "./static/css/main.bdf82220.chunk.css"
+  },
+  {
+    "revision": "4b964fc25ecc0b658447",
+    "url": "./static/js/2.18166693.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18166693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2d8d11faeb91ad67fd",
+    "url": "./static/js/main.386ba13e.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
